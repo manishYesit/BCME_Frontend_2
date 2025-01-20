480c9664b4af9ec24afc8f96c4a81b882f6ff68b
manishYesit <yesitlabs.manishkumar@gmail.com>
2025-01-20 16:02:38 +0530

    Fixed pathnames

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -123,6 +123,7 @@ export default function RootLayout({
                           onClick={(e) => {
                             handleLiClick(e);
                             handleChangePassword();
+                            setIsOpen(prev => !prev);
                           }}
                           style={{ cursor: "pointer" }}
                         >
@@ -246,7 +247,7 @@ export default function RootLayout({
                     href="/admin/roof_list"
                     className={
                       "nav-link " +
-                      (pathName == "/admin/roof_list" ? "active" : "")
+                      ((pathName.startsWith("/admin/roof_list") || pathName === "/admin/roof_set_image") ? "active" : "")
                     }
                   >
                     <i className="nav-icon fa-solid fa-people-roof"></i>
@@ -258,7 +259,7 @@ export default function RootLayout({
                     href="/admin/stair_list"
                     className={
                       "nav-link " +
-                      (pathName == "/admin/stair_list" ? "active" : "")
+                      ((pathName.startsWith("/admin/stair_list") || pathName === "/admin/stair_set_image") ? "active" : "")
                     }
                   >
                     <i className="nav-icon fa-solid fa-stairs"></i>
